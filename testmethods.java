13b370360ca91c6d798cf3f4146b399a71135fc3
nrocpop <tarobinsontoast@gmail.com>
2021-10-15 23:40:33 -0400

    Test methods added for sending files of any size

--- testmethods.java
+++ testmethods.java
@@ -21,33 +21,45 @@ public class testmethods {
 //    fos.close();
 //}
 
+//    public static String EncodeBase64(File f)throws IOException{
+//    FileInputStream fis = new FileInputStream(f);
+//    byte[] fileBytes =new byte[(int)f.length()];
+//    fis.read(fileBytes);
+//    String encodedFile = Base64.getEncoder().encodeToString(fileBytes);
+//    return encodedFile;
+//}
 
+public static String EncodeBase64(byte[] b)throws IOException{
+    return Base64.getEncoder().encodeToString(b);
 
-    public static String EncodeBase64(File f)throws IOException{
-    FileInputStream fis = new FileInputStream(f);
-    byte[] fileBytes =new byte[(int)f.length()];
-    fis.read(fileBytes);
-    String encodedFile = Base64.getEncoder().encodeToString(fileBytes);
-
-    return encodedFile;
 }
-
 
 public static byte[] DecodeBase64(String fileString){
-       byte[] decoded = Base64.getDecoder().decode(fileString);
-       return decoded;
-
+      return Base64.getDecoder().decode(fileString);
 }
     public static void main(String[] args)throws IOException {
-        File myFile = new File("C:\\Users\\Alex\\Desktop\\Donut1440.png");
-       String encoded = EncodeBase64(myFile);
-       System.out.print(encoded);
-       byte[] arr = DecodeBase64(encoded);
-        File newFile = new File("C:\\Users\\Alex\\Desktop\\marscopy.png");
-        FileOutputStream fos = new FileOutputStream(newFile);
-        fos.write(arr);
-        fos.flush();
+
+        File myFile = new File("C:\\Users\\xbato\\Desktop\\T3.mkv");
+        File myCopy = new File ("C:\\Users\\xbato\\Desktop\\myCopy.mkv");
+        FileInputStream fis = new FileInputStream(myFile);
+        byte[] partToSend = new byte[1024];
+        byte[] partRcv = new byte[1024];
+        FileOutputStream fos = new FileOutputStream(myCopy);
+        String Encoded;
+        Long current = 0l;
+        Long FileSize = myFile.length();
+
+        while(current <= FileSize) {
+
+            fis.read(partToSend);
+            Encoded = EncodeBase64(partToSend);
+            partRcv = DecodeBase64(Encoded);
+            fos.write(partRcv);
+            fos.flush();
+            current += 1024l;
+        }
         fos.close();
+        System.out.print("FIN");
 
 
     }
