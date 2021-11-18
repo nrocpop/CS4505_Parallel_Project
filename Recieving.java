2d2e1777cce713a19228e30f6c0accb7ed701d96
nrocpop <tarobinsontoast@gmail.com>
2021-11-18 15:09:24 -0500

    Decoder implemented

---
+++ Recieving.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+
+public class Recieving extends Thread{
+    ClientView Mother = null;
+    Recieving(ClientView parent){
+        Mother = parent;
+    }
+    public void run() {
+
+    }
+}
